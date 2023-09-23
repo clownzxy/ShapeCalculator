e884a33783250d54170190ea3f5cbe1c8075475e
clownzxy <norwen.penas.21@usjr.edu.ph>
2023-09-23 19:50:39 +0800

    test: Trying to retrieve data for Calculate Button

---
+++ PenasLab3/Page/ShapeData.cs
@@ -0,0 +1,48 @@
+namespace PenasLab3;
+
+public class ShapeData
+{
+
+    private int _input1;
+    private int _input2;
+    private int _input3;
+
+    public int Input1
+    {
+        get => _input1;
+        set
+        {
+            _input1 = value;
+        }
+    }
+
+    public int Input2
+    {
+        get => _input2;
+        set
+        {
+            _input2 = value;
+        }
+    }
+
+    public int Input3
+    {
+        get => _input3;
+        set
+        {
+            _input3 = value;
+        }
+    }
+
+    public class TriangleCalculate : ShapeData
+    {
+        public int AreaOfTriangle()
+        {
+            return (Input1 * Input2 / 2);
+        }
+    }
+
+}
+
+
+
--- PenasLab3/Page/Triangle.xaml.cs
+++ PenasLab3/Page/Triangle.xaml.cs
@@ -1,11 +1,50 @@
+using System.Collections.ObjectModel;
+
 namespace PenasLab3.Page;
 
 public partial class Triangle : ContentPage
 {
+	private ObservableCollection<String> _unitPicker;
+
+	public ObservableCollection<String> UnitPicker
+	{
+		get => _unitPicker;
+		set
+		{
+			_unitPicker=value;
+		}
+	}
+
 	public Triangle()
 	{
 		InitializeComponent();
+
+		UnitPicker = new ObservableCollection<String>
+		{
+			"--SELECT--",
+			"cm"
+		};
+
+
+		BindingContext = this;
 	}
 
+	private ShapeData DataRegister()
+	{
+		return new ShapeData
+		{
+			Input1 = int.Parse(txtBase.Text),
+			Input2 = int.Parse(txtHeight.Text)
+	};
+	}
 
+   
+
+    private void OnCalculateButtonClick(object sender, EventArgs e)
+	{
+		TriangleCalculate newCalculator = new();
+		DataRegister();
+		txtResult.Text = newTriangle.AreaOfTriangle().ToString();
+
+    }
 }
